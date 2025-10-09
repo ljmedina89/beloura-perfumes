19bb5f08f605272d7c2c9c05ce8424007b22c026
ljmedina89 <leomedinaricachi@gmail.com>
2025-10-08 23:27:44 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// Cambia el base al nombre EXACTO del repo de GitHub Pages
 export default defineConfig({
-  plugins: [react()],
-  base: '/beloura-perfumes/',
+  base: '/', // ✅ importante cuando usas dominio propio
 })
